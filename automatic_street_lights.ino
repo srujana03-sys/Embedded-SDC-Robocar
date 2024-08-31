18459c538b78e076b227ede632694e1663b0669b
srujana03-sys <yarajarlasrujana@gmail.com>
2024-08-31 06:02:46 -0400

    AUTOMATIC STREET LIGHTS USING LDR SENSOR

---
+++ automatic_street_lights.ino
@@ -0,0 +1,21 @@
+void setup() {
+  pinMode(13,OUTPUT);
+  pinMode(A0,INPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int sw;
+  sw=analogRead(A0);
+  Serial.println(sw);
+  if(sw<500)
+  {
+    digitalWrite(13,HIGH);
+  }
+  else
+  {
+    digitalWrite(13,LOW);
+  }
+}
